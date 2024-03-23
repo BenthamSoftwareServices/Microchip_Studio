be58a56975f4add2378320bdc3370840cc2f02d6
Steven Bennett <benthamsoftwareservices@gmail.com>
2024-03-23 17:11:25 +0000

    Initial commit

---
+++ 7.0/AVR/Hello_World_1602_LCD/Hello_World_1602_LCD/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 01/04/2023 12:43:24
+ * Author : Steven
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
---
+++ 7.0/AVR/Hello_World_1602_LCD/Hello_World_1602_LCD/Hello_World_1602_LCD/Sketch.cpp
@@ -0,0 +1,25 @@
+﻿//using the "" first checks your local directory, and if it doesn't find a match then moves on to check the system paths. Using <> starts the search with system headers.
+#include "Arduino.h"
+#include <LiquidCrystal_I2C_Hangul.h>
+#include <Wire.h>
+
+LiquidCrystal_I2C_Hangul lcd(0x27,16,2); //I2C address in hex, followed by number of columns and rows
+
+void setup() {
+  Serial.begin(9600);
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+  delay(1000);
+  lcd.print("Hello Steve!");
+  Serial.println ("Setup complete");
+}
+
+void loop() {
+	delay(1000);
+	lcd.setCursor(0,1); //Column starting at 0, Row 0 for top and 1 for bottom
+	lcd.print("Bottom row");
+	delay(1000);
+	lcd.clear();
+	Serial.println("Loop running");
+}
